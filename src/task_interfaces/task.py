5ba2be67b292ddf1cc74e3409481fb01d68bf64a
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-07-08 16:21:04 -0400

    feat: add code analysis task

--- src/task_interfaces/task.py
+++ src/task_interfaces/task.py
@@ -89,6 +89,10 @@ class FormatTask(BaseTask):
 class StaticAnalysisTask(FormatTask):
     can_fix: bool = False
 
+class CodeAnalysisTask(BaseTask):
+    type = TaskTypes.CODE_ANALYSIS
+    source_script_path: str
+    handler: str = "task"
 
 class TaskInterface(ABC):
     command: str = ""
